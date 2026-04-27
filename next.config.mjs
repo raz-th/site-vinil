abe48288b5531099d53a732560b5d7a6f11b4bb4
raz-th <raz.taz032023@gmail.com>
2026-04-27 21:37:45 +0300

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.1.29', '10.145.79.155', "*"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // output: "export"
 };
 
